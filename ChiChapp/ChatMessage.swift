a3ffb083b38b7ab96b9df914b95ad1113e8c0ac0
Alvar Aronija <aronija_1@hotmail.com>
2018-04-18 15:53:06 +0200

    Send chat message works

---
+++ ChiChapp/ChatMessage.swift
@@ -0,0 +1,42 @@
+//
+//  ChatMessage.swift
+//  ChiChapp
+//
+//  Created by Alvar Aronija on 18/04/2018.
+//  Copyright © 2018 Alvar Aronija. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+
+struct ChatMessage: MessageType {
+    var sender: Sender
+    var messageId: String
+    var sentDate: Date
+    var data: MessageData
+    
+    init(sender: Sender, messageId: String, date: Date, data: MessageData) {
+        self.sender = sender
+        self.messageId = messageId
+        self.sentDate = date
+        self.data = data
+    }
+    
+    init(text: String, sender: Sender, messageId: String, date: Date) {
+        self.init(sender: sender, messageId: messageId, date: date, data: .text(text))
+    }
+    
+    init(attributedText: NSAttributedString, sender: Sender, messageId: String, date: Date) {
+        self.init(sender: sender, messageId: messageId, date: date, data: .attributedText(attributedText))
+    }
+    
+    init(image: UIImage, sender: Sender, messageId: String, date: Date) {
+        self.init(sender: sender, messageId: messageId, date: date, data: .photo(image))
+    }
+    
+    init(emoji: String, sender: Sender, messageId: String, date: Date) {
+        self.init(sender: sender, messageId: messageId, date: date, data: .emoji(emoji))
+    }
+}
+
+
--- ChiChapp/ChatViewController.swift
+++ ChiChapp/ChatViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 import MessageKit
 
-class ChatViewController: MessagesViewController  {
-
+class ChatViewController: MessagesViewController {
+    
+    
+    var messages: [MessageType] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+        
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +31,9 @@ class ChatViewController: MessagesViewController  {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: MessageDataSource delegate methods
+    
+
 
     /*
     // MARK: - Navigation
@@ -32,5 +44,56 @@ class ChatViewController: MessagesViewController  {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension ChatViewController: MessagesDataSource {
+    
+    func currentSender() -> Sender {
+        return Sender(id: "1234", displayName: "Alvar")
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+        
+    }
+    
+    func numberOfMessages(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+}
+
+extension ChatViewController: MessagesLayoutDelegate {
+    
+    func heightForLocation(message: MessageType, at indexPath: IndexPath, with maxWidth: CGFloat, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 200
+    }
+}
+
+extension ChatViewController: MessagesDisplayDelegate {
+    
+}
+
+extension ChatViewController: MessageInputBarDelegate {
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        print("Send button clicked")
+        for component in inputBar.inputTextView.components {
+            
+            if let image = component as? UIImage {
+                let imageMessage = ChatMessage(image: image, sender: currentSender(), messageId: UUID().uuidString, date: Date())
+                messages.append(imageMessage)
+                messagesCollectionView.insertSections([messages.count - 1])
+            } else if let text = component as? String {
+                
+                let attributedText = NSAttributedString(string: text, attributes: [.font: UIFont.systemFont(ofSize: 15), .foregroundColor: UIColor.blue ])
+                
+                let message = ChatMessage(attributedText: attributedText, sender: currentSender(), messageId: UUID().uuidString, date: Date())
+                messages.append(message)
+                print("Amount of messages \(messages.count)")
+                messagesCollectionView.insertSections([messages.count - 1])
+            }
+        }
+        inputBar.inputTextView.text = String()
+        messagesCollectionView.scrollToBottom()
+    }
 }
